2a422dfd932bfe4c00cd9fa0d8179e4ab5cf89d9
kin-tama <65250935+kin-tama@users.noreply.github.com>
2020-10-28 23:32:51 +0300

    сделал учебное задание

--- js/setup.js
+++ js/setup.js
@@ -2,7 +2,6 @@
 // показываем "setup"
 
 let userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 // нашли куда вставлять
@@ -23,6 +22,8 @@ const WIZARD_NAMES = ["Иван ", "Хуан Себастьян ", "Мария "
 const FAMILY_NAMES = ["да Марья", "Верон", "Мирабелла", "Вальц", "Онопко", "Топольницкая", "Нионго", "Ирвинг"];
 const COAT_COLORS = ["rgb(101, 137, 164)", "rgb(241, 43, 107)", "rgb(146, 100, 161)", "rgb(56, 159, 117)", "rgb(215, 210, 55)", "rgb(0, 0, 0)"];
 const EYE_COLORS = ["black", "red", "blue", "yellow", "green"];
+const FIREBALL_COLORS = ["#ee4830", "#30a8ee", "#5ce6c0", "#e848d5", "#e6e848"];
+
 let wizards = [];
 
 for (let i = 0; i < 4; i++) {
@@ -42,3 +43,86 @@ for (let i = 0; i < wizards.length; i++) {
   wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
   similarListElement.appendChild(wizardElement);
 }
+
+// module4-task1
+// ниже блок по открыванию-закрыванию формы
+const setupCloseButton = document.querySelector(".setup-close");
+const setupOpenIcon = document.querySelector(".setup-open-icon");
+
+const closeOnEsc = function (evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    userDialog.classList.add('hidden');
+  }
+};
+
+const closeOnEnter = function (evt) {
+  if (evt.key === 'Enter') {
+    evt.preventDefault();
+    userDialog.classList.add('hidden');
+  }
+};
+
+let openSetup = function () {
+  userDialog.classList.remove("hidden");
+  document.addEventListener('keydown', closeOnEsc);
+  setupCloseButton.addEventListener('keydown', closeOnEnter);
+};
+
+let closeSetup = function () {
+  userDialog.classList.add("hidden");
+  document.removeEventListener('keydown', closeOnEsc);
+  setupCloseButton.removeEventListener('keydown', closeOnEnter);
+};
+
+setupOpenIcon.addEventListener("click", function () {
+  openSetup();
+});
+
+setupOpenIcon.addEventListener("keydown", function (evt) {
+  if (evt.key === "Enter") {
+    openSetup();
+  }
+});
+
+setupCloseButton.addEventListener("click", function () {
+  closeSetup();
+});
+
+// ниже блок смене цвета колдуна
+
+const wizardCoat = document.querySelector(".setup-wizard .wizard-coat");
+const wizardEyes = document.querySelector(".wizard-eyes");
+const wizardFireball = document.querySelector(".setup-fireball-wrap");
+
+// функция смены цвета пальта
+const changeCoatColor = function () {
+  wizardCoat.style.fill = COAT_COLORS[getRandomInteger(0, COAT_COLORS.length)];
+  document.querySelector("#s-coat-color").value = wizardCoat.style.fill;
+};
+
+// функция смены цвета глаз
+const changeEyeColor = function () {
+  wizardEyes.style.fill = EYE_COLORS[getRandomInteger(0, EYE_COLORS.length)];
+  document.querySelector("#s-eyes-color").value = wizardEyes.style.fill;
+};
+
+// функция смены цвета шара
+const changeFireballColor = function () {
+  wizardFireball.style.backgroundColor = FIREBALL_COLORS[getRandomInteger(0, FIREBALL_COLORS.length)];
+  wizardFireball.querySelector("input").value = wizardFireball.style.backgroundColor;
+};
+
+// обработчики
+
+wizardCoat.addEventListener("click", function () {
+  changeCoatColor();
+});
+
+wizardFireball.addEventListener("click", function () {
+  changeFireballColor();
+});
+
+wizardEyes.addEventListener("click", function () {
+  changeEyeColor();
+});
